c37dd4e63a47f56d22161565b04cefdd5033aead
Zarifa <mamadalievazara@gmail.com>
2025-08-27 00:32:54 +0200

    Add: theme switch function to the JS. Aplly background img to the Spawn theme.

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
+function chooseHero(hero) {
+    localStorage.setItem("theme", hero);
+    window.location.href = "index.html";
+}
 
-
+const savedTheme = localStorage.getItem("theme") || "spawn"; 
+document.getElementById("theme-link").setAttribute("href", `css/${savedTheme}.css`);
