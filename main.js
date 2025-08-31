a0a6394e64cce9eaec9e9c696923b980364ba61a
Zarifa <mamadalievazara@gmail.com>
2025-08-31 22:23:54 +0200

    Create functions for basic math operantions

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ function chooseHero(hero) {
 
 const savedTheme = localStorage.getItem("theme") || "spawn"; 
 document.getElementById("theme-link").setAttribute("href", `css/${savedTheme}.css`);
+
+const Btns = document.querySelector('.buttons')
+
+const add = function(a, b) {
+    return = a + b;
+}
+
+const subtract = function(a, b) {
+    return = a - b;
+}
+
+const multiply = function(numbers) {
+    return = numbers.reduce((acc, current) => acc * current);
+}
+
+const divide = function(numbers) {
+    return = numbers.reduce((acc, current) => acc / current);
+}
