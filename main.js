37c453ead1b9bd4c549af4df292ed58bfcfab4c2
Zarifa <mamadalievazara@gmail.com>
2025-08-31 23:55:28 +0200

    Add: opearate function to call add/subtract/multiply etc. Unable to make it work. Function calls itself.

--- main.js
+++ main.js
@@ -6,20 +6,39 @@ function chooseHero(hero) {
 const savedTheme = localStorage.getItem("theme") || "spawn"; 
 document.getElementById("theme-link").setAttribute("href", `css/${savedTheme}.css`);
 
-const Btns = document.querySelector('.buttons')
+const Btns = document.querySelector('.buttons');
 
 const add = function(a, b) {
-    return = a + b;
+    return a + b;
 }
 
 const subtract = function(a, b) {
-    return = a - b;
+    return a - b;
 }
 
 const multiply = function(numbers) {
-    return = numbers.reduce((acc, current) => acc * current);
+    return numbers.reduce((acc, current) => acc * current);
 }
 
 const divide = function(numbers) {
-    return = numbers.reduce((acc, current) => acc / current);
+    return numbers.reduce((acc, current) => acc / current);
 }
+
+let firstOperand = [];
+let operator = [];
+let secondOperand = [];
+
+function operate(input) {
+    if (input == '+') {
+        return add;
+    } else if (input == '-') {
+        return subtract;
+    } else if (input == '*') {
+        return multiply;
+    } else {
+        return divide;
+    }
+}
+
+operate(operator);
+
