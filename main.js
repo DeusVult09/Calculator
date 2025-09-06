e10c5fdf12ed552eb41ea28802c4d248848879dd
Zarifa <mamadalievazara@gmail.com>
2025-09-06 18:36:10 +0200

    Style: spawn theme circular shaped calc and add simple animations

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-function chooseHero(hero) {
-    localStorage.setItem("theme", hero);
-    window.location.href = "index.html";
-}
-
-const savedTheme = localStorage.getItem("theme") || "spawn";
-document.getElementById("theme-link").setAttribute("href", `css/${savedTheme}.css`);
 
 const calDisplay = document.getElementById("display");
 const btns = document.querySelector(".buttons");
